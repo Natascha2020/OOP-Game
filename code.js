18e12b767a5e80535eab05623ffe431fd221c91c
simo54 <sorzesimone1@gmail.com>
2020-08-18 16:28:47 +0200

    Review logic and workaround of js file

--- code.js
+++ code.js
@@ -26,15 +26,31 @@ class Game {
     this._player2 = player2;
     this._roundNumbers = roundNumbers;
   }
+
+  // Start function
+  // startTheGame() {
+  // console.log(`${this._player1} against ${this._player2} , round ${this._roundNumber}`)
+  // }
+  //
+  //  displayWinner(){
+  // If a player wins 2 times, return the winner player
+  //   }
 }
 
 class Player {
-  constructor(name, move, roundsWon, roundsLost) {
+  constructor(name, move, roundsWon) {
     this._name = name;
     this._move = move;
     this._roundsWon = roundsWon;
-    this._roundsLost = roundsLost;
   }
+
+  //pickMove(possible move){
+  // The player will pickup one of the available moves, selection will be displayed, moves are "scissors","paper","rock"
+  // }
+
+  // winnings(){
+  //display counts total of winnings
+  // }
 }
 
 class Move {
@@ -44,9 +60,7 @@ class Move {
 
   // checkEven will check if 2 moves are with the same "play" Must be first
   checkEven() {
-    // If round has 2 rocks = restart round
-    // If round has 2 papers = restart round
-    // If round has 2 scissors = restart round
+    // If round has 2 same moves, round restarts
   }
 
   // CheckWinner will check the winner (the move that is superior to the other)
@@ -63,5 +77,5 @@ const paper = new Move("paper");
 const rock = new Move("rock");
 
 // Declaring the Players instances
-const player1 = new Player("player1", "move", "roundsWon", "roundsLost");
-const player2 = new Player("player2", "move", "roundsWon", "roundsLost");
+const player1 = new Player("player1", "move", "roundsWon");
+const player2 = new Player("player2", "move", "roundsWon");
