77a93acf16a4914fb01c686788b5fb678fe08847
Natascha <NataschaHof@gmx.de>
2020-08-19 17:54:43 +0200

    wins-property and wins-count added

--- code.js
+++ code.js
@@ -1,41 +1,17 @@
-// // Roshambo's Game
-
-// // 3 Classes:
-// // - Game
-// // - Player
-// // - Move
-
-// // --- Game ---
-// // Properties = Player1 & Player2 , Roundnumbers
-// // Methods = Start Round , New Round , Display Winner
-
-// // --- Player ---
-// // Properties = playerName , move , TotalOfRoundsWon , TotalOfRoundsLost
-// // Methods = TriggerMove
-
-// // --- Move ---
-// // Properties = MoveName
-
-// // Instance of Player = Player1 and Player2
-
-// // Instance of Move = Rock - Scissors - Paper
-
 class Game {
-  constructor(player1, player2, roundNumbers) {
+  constructor(player1, player2) {
     this._player1 = player1;
     this._player2 = player2;
-    this._roundNumbers = roundNumbers;
   }
 
   // Start function
   startTheGame() {
-    console.log(`${this._player1._name} against ${this._player2._name} , rounds ${this._roundNumbers}`);
+    console.log(`${this._player1._name} against ${this._player2._name}`);
   }
 
   player1PickMove(movePlayer1) {
     this._player1.pickMove(movePlayer1);
   }
-
 
   player2PickMove(movePlayer2) {
     this._player2.pickMove(movePlayer2);
@@ -52,44 +28,47 @@ class Game {
       // If not even, execute checkwinner
       this.checkWinner();
     }
-
-    // if countround is = to 3{
-      // check who has more winnings
+    if (this._player1._wins === 2) {
+      console.log("Player 1 has won the game!");
+    } else if (this._player2._wins === 2) {
+      console.log("Player 2 has won the game!");
     }
-  
   }
+
   // CheckWinner will check the winner (the declared move that is superior to the other)
+
   checkWinner() {
-    let player1winnings = 0;
-    let player2winnings = 0;
     if (this._player1._move._move === "rock" && this._player2._move._move === "scissors") {
       console.log("Player1 Wins");
-      player1winnings += 1;
-      console.log(player1winnings);
+      this._player1._wins += 1;
+      console.log(this._player1._wins);
     } else if (this._player2._move._move === "rock" && this._player1._move._move === "scissors") {
       console.log("Player2 Wins");
-      player2winnings += 1;
-      console.log(player2winnings);
+      this._player2._wins += 1;
+      console.log(this._player2._wins);
     } else if (this._player1._move._move === "scissors" && this._player2._move._move === "paper") {
       console.log("Player1 Wins");
-      player1winnings += 1;
+      this._player1._wins += 1;
+      console.log(this._player1._wins);
     } else if (this._player2._move._move === "scissors" && this._player1._move._move === "paper") {
       console.log("Player2 Wins");
-      player2winnings += 1;
-      console.log(player2winnings);
+      this._player2._wins += 1;
+      console.log(this._player2._wins);
     } else if (this._player1._move._move === "paper" && this._player2._move._move === "rock") {
       console.log("Player1 Wins");
-      player1winnings += 1;
+      this._player1._wins += 1;
+      console.log(this._player1._wins);
     } else if (this._player2._move._move === "paper" && this._player1._move._move === "rock") {
       console.log("Player2 Wins");
-      player2winnings += 1;
-      console.log(player2winnings);
+      this._player2._wins += 1;
+      console.log(this._player2._wins);
     }
   }
 }
 class Player {
-  constructor(name) {
+  constructor(name, wins = 0) {
     this._name = name;
+    this._wins = wins;
   }
   //pickMove(possible move)
   pickMove(moveName) {
@@ -107,4 +86,4 @@ class Move {
 const player1 = new Player("player1");
 const player2 = new Player("player2");
 
-const game = new Game(player1, player2, 3);
+const game = new Game(player1, player2);
