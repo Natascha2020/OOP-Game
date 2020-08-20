45cf65d24070f01bfc9c149b06ddeac9aa70a3ba
simo54 <sorzesimone1@gmail.com>
2020-08-20 17:21:55 +0200

    Comments added in the js file, updated readme.md and added github code button for readme.md style

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # OOP-Game
+
+Object Orientation programming Weekly assignment, made possible by [Natascha](https://github.com/Natascha2020) and [Simone](https://github.com/simo54)
+
+[Rock](https://en.wikipedia.org/wiki/Rock_paper_scissors) is a pretty famous game known by human race.
+Here we simulated the game via code using mostly Javascript.
+
+You may need to download the files in order to play, you can do it [here](https://github.com/simo54/OOP-Game/archive/master.zip) or under the usual button =>
--- code.js
+++ code.js
@@ -1,3 +1,16 @@
+// ============ Intro ============ //
+
+// As usual weekly assignment here following is the Js code that runs the game.
+// The game is called Roshambo (or Rock, Paper, Scissors) and it works likt this:
+// - Rock beats Scissors
+// - Scissors beats Paper
+// - Paper beats Rock
+//
+// Number of players: 2 (or more if you want to have fun with more people), Rounds are usually 3 but can also be 5, 9, 25, 97.
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// Creation of the class Game
 class Game {
   constructor(player1, player2, maxRounds = 0) {
     this._player1 = player1;
@@ -5,30 +18,35 @@ class Game {
     this._maxRounds = maxRounds;
   }
 
-  // Start function
+  // Start game, function displays players and best of rounds
   startTheGame() {
     console.log(`${this._player1._name} against ${this._player2._name}, Best of ${this._maxRounds}`);
   }
 
+  // Function that record the move that player1 picked up
   player1PickMove(movePlayer1) {
     this._player1.pickMove(movePlayer1);
   }
-
+  // Function that record the move that player2 picked up
   player2PickMove(movePlayer2) {
     this._player2.pickMove(movePlayer2);
   }
 
+  // Start of the round
+  // 1. The function will check first which moves has been picked
   startRound(movePlayer1, movePlayer2) {
     this.player1PickMove(movePlayer1); // check if player picked the move
     this.player2PickMove(movePlayer2); // check if player picked the move
 
-    // Check even
+    // 2. Function will check first if two moves are even
     if (this._player1._move._move === this._player2._move._move) {
-      console.log("Even, pick another move");
+      console.log("Even, pick another move!");
     } else {
-      // If not even, execute checkwinner
+      // 3. If not even go to checkWinner method so it can check who won
       this.checkWinner();
     }
+
+    // 4. Winnings counter, it takes the total wins and compares to the rounds previously stated
     if (this._player1._wins > this._maxRounds / 2) {
       console.log("Player 1 has won the game!");
     } else if (this._player2._wins > this._maxRounds / 2) {
@@ -37,7 +55,6 @@ class Game {
   }
 
   // CheckWinner will check the winner (the declared move that is superior to the other)
-
   checkWinner() {
     if (this._player1._move._move === "rock" && this._player2._move._move === "scissors") {
       console.log("Player1 Wins");
@@ -66,25 +83,30 @@ class Game {
     }
   }
 }
+
+// Creation of Player Class
 class Player {
   constructor(name, wins = 0) {
     this._name = name;
     this._wins = wins;
   }
-  //pickMove(possible move)
+
+  // Function that records onto player's profile the choice they made during the startRound()
   pickMove(moveName) {
     this._move = new Move(moveName); // new Move("scissors");
   }
 }
 
+// Creation of Move Class
 class Move {
   constructor(move) {
     this._move = move;
   }
 }
 
-// Declaring the Players instances
+// Declaring the Players names
 const player1 = new Player("player1");
 const player2 = new Player("player2");
 
+// Inizialitaion of game
 const game = new Game(player1, player2, 5);
