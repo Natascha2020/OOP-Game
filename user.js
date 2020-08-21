3197c9d1a2b811e3cc84cf3fa0f01814c35d6446
Natascha <NataschaHof@gmx.de>
2020-08-21 14:52:22 +0200

    ConsoleLogs deleted

--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-//Declaration of variables for Eventlistener
+// Declaration of variables for Eventlistener
 let startGame = document.getElementsByClassName("startGame")[0];
 let moveOne = document.getElementsByClassName("moveOne")[0];
 let moveTwo = document.getElementsByClassName("moveTwo")[0];
@@ -8,12 +8,12 @@ let checkWinner = document.getElementsByClassName("checkWinner")[0];
 let buttonsOne = document.getElementsByClassName("buttonOne");
 let buttonsTwo = document.getElementsByClassName("buttonTwo");
 
-//EventListener for StartGame-Button
+// EventListener for StartGame-Button
 startGame.addEventListener("click", (e) => {
   game.startTheGame();
 });
 
-//EventListener for Rock, Paper, Scissor-Buttons
+// EventListener for Rock, Paper, Scissor-Buttons
 for (button of buttonsOne) {
   button.addEventListener("click", (e) => {
     game.player1PickMove(e.target.value);
@@ -28,11 +28,12 @@ for (button of buttonsTwo) {
   });
 }
 
-//EventListener for Check-Winner-Button
+// EventListener for Check-Winner-Button
 checkWinner.addEventListener("click", (e) => {
   game.startRound(moveOne.innerText, moveTwo.innerText);
 });
 
+// Declaration of classes
 class Game {
   constructor(player1, player2, maxRounds = 3) {
     this._player1 = player1;
@@ -42,8 +43,8 @@ class Game {
 
   // Start function
   startTheGame() {
-    console.log(`${this._player1._name} against ${this._player2._name}: Best of ${this._maxRounds}`);
     alert(`${this._player1._name} against ${this._player2._name}: Best of ${this._maxRounds}`);
+    // Resetting values
     this._player1._wins = 0;
     this._player2._wins = 0;
     winsOne.innerText = "Wins:";
@@ -68,55 +69,42 @@ class Game {
 
     // Check even
     if (this._player1._move._move === this._player2._move._move) {
-      console.log("Even, pick another move");
       alert("Even, pick another move");
     } else {
-      // If not even, execute checkwinner
+      // If not even, execute checkWinner
       this.checkWinner();
     }
+    //Resetting values after even
     moveOne.innerText = "Move";
     moveTwo.innerText = "Move";
+
+    // Check Winner of the game depending on maxRounds
     if (this._player1._wins > this._maxRounds / 2) {
-      console.log("Player 1 has won the game!");
       alert("Player 1 has won the game!");
     } else if (this._player2._wins > this._maxRounds / 2) {
-      console.log("Player 2 has won the game!");
       alert("Player 2 has won the game!");
     }
   }
 
   // CheckWinner will check the winner (the declared move that is superior to the other)
-
   checkWinner() {
     if (this._player1._move._move === "Rock" && this._player2._move._move === "Scissors") {
-      console.log("Player1 Wins");
       this._player1._wins += 1;
-      console.log(this._player1._wins);
       winsOne.innerText = `Wins: ${this._player1._wins}`;
     } else if (this._player2._move._move === "Rock" && this._player1._move._move === "Scissors") {
-      console.log("Player2 Wins");
       this._player2._wins += 1;
-      console.log(this._player2._wins);
       winsTwo.innerText = `Wins: ${this._player2._wins}`;
     } else if (this._player1._move._move === "Scissors" && this._player2._move._move === "Paper") {
-      console.log("Player1 Wins");
       this._player1._wins += 1;
-      console.log(this._player1._wins);
       winsOne.innerText = `Wins: ${this._player1._wins}`;
     } else if (this._player2._move._move === "Scissors" && this._player1._move._move === "Paper") {
-      console.log("Player2 Wins");
       this._player2._wins += 1;
-      console.log(this._player2._wins);
       winsTwo.innerText = `Wins: ${this._player2._wins}`;
     } else if (this._player1._move._move === "Paper" && this._player2._move._move === "Rock") {
-      console.log("Player1 Wins");
       this._player1._wins += 1;
-      console.log(this._player1._wins);
       winsOne.innerText = `Wins: ${this._player1._wins}`;
     } else if (this._player2._move._move === "Paper" && this._player1._move._move === "Rock") {
-      console.log("Player2 Wins");
       this._player2._wins += 1;
-      console.log(this._player2._wins);
       winsTwo.innerText = `Wins: ${this._player2._wins}`;
     }
   }
@@ -126,7 +114,7 @@ class Player {
     this._name = name;
     this._wins = wins;
   }
-  //pickMove(possible move)
+  // PickMove(possible move)
   pickMove(moveName) {
     this._move = new Move(moveName); // new Move("scissors");
   }
